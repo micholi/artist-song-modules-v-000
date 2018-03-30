3f0357ee1d89b9fd5b9a90c259ca2d3aab9bed1c
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 22:55:08 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,13 +1,9 @@
 module Memorable
 
-# Reminder: self keyword is omitted when defining class methods inside modules
-
-# Reset Method
   def reset_all
     all.clear
   end
 
-# Count Method
   def count
     all.count
   end
