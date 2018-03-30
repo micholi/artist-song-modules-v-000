7104171a6e7fbba62d41cada5b586c7b951e040d
Michelle Olivieri <michelleo72@gmail.com>
2018-03-30 22:22:45 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,21 @@
 module Memorable
 
+# Artist Methods
+  # def self.reset_all
+  #  self.all.clear
+  # end
+
+  # def self.count
+  #  @@artists.count
+  # end
+
+# Song Methods
+  # def self.reset_all
+  #  @@songs.clear
+  # end
+
+  # def self.count
+  #  self.all.count
+  # end
 
 end
